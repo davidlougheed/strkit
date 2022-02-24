92d4bffb6efb7d8d25690214de3348cddc468201
David Lougheed <david.lougheed@gmail.com>
2022-02-24 11:32:02 -0500

    fix: type error in gangstr catalog conversion

--- stronger/convert/gangstr.py
+++ stronger/convert/gangstr.py
@@ -7,4 +7,4 @@ __all__ = [
 
 def trf_bed_to_gangstr(trf_data: list):
     for i, item in enumerate(trf_data, 1):
-        sys.stdout.write("\t".join((*item[:3], item[-1], len(item[-1]))) + "\n")
+        sys.stdout.write("\t".join((*item[:3], item[-1], str(len(item[-1])))) + "\n")
