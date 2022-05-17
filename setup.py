e610fa676b8c9fddccd6baddefb92b1fad10eac3
David Lougheed <david.lougheed@gmail.com>
2022-05-17 19:40:54 -0400

    chore: update sklearn

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         "pysam>=0.16.0.1,<0.20",
         "numpy>=1.20,<=1.22",
         "parasail>=1.2.4,<1.3",
-        "scikit-learn>=1.0,<1.1",
+        "scikit-learn>=1.1,<1.2",
     ],
 
     description="A toolkit for analyzing variation in short(ish) tandem repeats.",
