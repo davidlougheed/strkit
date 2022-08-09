c1a4bd96189c0bb2c9aaea8e59e31214d7e4a223
David Lougheed <david.lougheed@gmail.com>
2022-08-09 09:12:00 -0400

    chore: update Flask, scipy

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ setup(
 
     python_requires="~=3.8",
     install_requires=[
-        "Flask>=2.1.3,<2.2",
+        "Flask>=2.2.2,<2.3",
         "orjson>=3.7.11,<3.8",
         "pysam>=0.16.0.1,<0.20",
         "numpy>=1.23,<=1.24",
         "parasail>=1.2.4,<1.3",
         "scikit-learn>=1.1,<1.2",
-        "scipy>=1.7,<1.9",
+        "scipy>=1.8,<1.10",
         "statsmodels>=0.13.2,<0.14",
     ],
 
