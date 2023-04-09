ead43e4522a5807a58d6da8d4749379af6122a63
David Lougheed <david.lougheed@gmail.com>
2023-04-09 11:01:35 -0400

    test[call]: add test for caller utils

---
+++ tests/test_caller_utils.py
@@ -0,0 +1,25 @@
+from strkit.call.caller.utils import find_pair_by_ref_pos_py, normalize_contig, round_to_base_pos
+
+#         A  A       T  T       C  G       C  C       C  C       A  A       A  A       A  C
+PAIRS = [(0, 1000), (1, 1001), (2, 1003), (3, 1004), (4, 1005), (5, 1006), (6, 1008), (7, 1009)]
+SNVS = ((1003, "C"), (1009, "A"))
+
+
+def test_find_pair_by_ref_pos_py():
+    assert find_pair_by_ref_pos_py(PAIRS, 1004) == (3, True)
+    assert find_pair_by_ref_pos_py(PAIRS, 1007) == (6, False)
+
+
+def test_normalize_contig():
+    assert normalize_contig("chr5", True) == "chr5"
+    assert normalize_contig("5", True) == "chr5"
+    assert normalize_contig("X", True) == "chrX"
+    assert normalize_contig("chr5", False) == "5"
+    assert normalize_contig("chrX", False) == "X"
+
+
+def test_round_to_base_pos():
+    assert round_to_base_pos(2.5, 3) == 8/3  # 2.666666
+    assert round_to_base_pos(1.5, 2) == 1.5
+    assert round_to_base_pos(1.3, 4) == 1.25
+    assert round_to_base_pos(5.9, 4) == 6
