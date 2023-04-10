c568434e1cd6cd2320e753b55e35ffdd609398fc
David Lougheed <david.lougheed@gmail.com>
2023-04-10 11:56:47 -0400

    lint[call]: fix type hints & comments in realign

--- strkit/call/caller/realign.py
+++ strkit/call/caller/realign.py
@@ -22,13 +22,13 @@ def realign_read(
     always_realign: bool,
     q: Optional[mp.Queue] = None,  # TODO: why was this optional, again...
     log_level: int = logging.WARNING,
-) -> Optional[list[tuple[Optional[int], Optional[int]]]]:
+) -> Optional[list[tuple[int, int]]]:
     # Have to re-attach logger in separate process I guess
 
     from strkit.logger import create_process_logger
     lg = create_process_logger(os.getpid(), log_level)
 
-    # flipped: 'ref sequence' as query here, since it should in general be shorter
+    # flipped: 'ref sequence' as query here, since it should in general be shorter (!)
     pr = parasail.sg_dx_trace_scan_sat(
         # fetch an extra base for the right flank coordinate check later (needs to be >= the exclusive coord)
         ref_seq, query_seq, realign_indel_open_penalty, 0, dna_matrix)
@@ -41,12 +41,12 @@ def realign_read(
 
     lg.debug(
         f"Realigned {rn} in locus {t_idx}{' (due to soft clipping)' if not always_realign else ''}: scored {pr.score}; "
-        f"CIGAR: {pr.cigar.decode.decode('ascii')}")
+        f"Flipped CIGAR: {pr.cigar.decode.decode('ascii')}")
     # noinspection PyTypeChecker
-    res: list[tuple[Optional[int], Optional[int]]] = [
-        # reverse to get (ref, query) instead of (query, ref) due to the flip
+    res: list[tuple[int, int]] = [
+        # reverse to get (query, ref) instead of (ref, query), due to the flip (!)
         tuple(reversed(p))
-        # query_start instead of ref_start, due to the flip
+        # query_start instead of ref_start, due to the flip (!)
         for p in get_aligned_pairs_from_cigar(decode_cigar(pr.cigar.seq), query_start=left_flank_coord)
         if p[0] is not None and p[1] is not None
     ]
