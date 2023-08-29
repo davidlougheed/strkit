dc7708cca69dfe12911a8b9f13135e70daf4e5b8
David Lougheed <david.lougheed@gmail.com>
2023-08-29 11:43:14 -0400

    refact(call): use map instead of comprehension; factor out queue-return fn

--- strkit/call/realign.py
+++ strkit/call/realign.py
@@ -12,6 +12,10 @@ min_realign_score_ratio: float = 0.95  # TODO: parametrize
 realign_indel_open_penalty: int = 7  # TODO: parametrize
 
 
+MatchedCoordPairList = list[tuple[int, int]]
+MatchedCoordPairListOrNone = Optional[MatchedCoordPairList]
+
+
 def realign_read(
     ref_seq: str,
     query_seq: str,
@@ -22,8 +26,13 @@ def realign_read(
     always_realign: bool,
     q: Optional[mp.Queue] = None,  # TODO: why was this optional, again...
     log_level: int = logging.WARNING,
-) -> Optional[list[tuple[int, int]]]:
+) -> MatchedCoordPairListOrNone:
     # Have to re-attach logger in separate process I guess
+
+    def ret_q(v: MatchedCoordPairListOrNone) -> MatchedCoordPairListOrNone:
+        if q:
+            q.put(v)
+        return v
 
     from strkit.logger import create_process_logger
     lg = create_process_logger(os.getpid(), log_level)
@@ -35,21 +44,21 @@ def realign_read(
 
     if pr.score < (th := min_realign_score_ratio * (flank_size * 2 * match_score - realign_indel_open_penalty)):
         lg.debug(f"Realignment for {rn} scored below threshold ({pr.score} < {th:.2f})")
-        if q:
-            q.put(None)
-        return None
+        return ret_q(None)
 
     lg.debug(
         f"Realigned {rn} in locus {t_idx}{' (due to soft clipping)' if not always_realign else ''}: scored {pr.score}; "
         f"Flipped CIGAR: {pr.cigar.decode.decode('ascii')}")
-    # noinspection PyTypeChecker
-    res: list[tuple[int, int]] = [
-        # reverse to get (query, ref) instead of (ref, query), due to the flip (!)
-        tuple(reversed(p))
-        # query_start instead of ref_start, due to the flip (!)
-        for p in get_aligned_pairs_from_cigar(
-            decode_cigar(pr.cigar.seq), query_start=left_flank_coord, matches_only=True)
-    ]
-    if q:
-        q.put(res)
-    return res
+
+    res: MatchedCoordPairList = list(map(
+        tuple,
+        map(
+            # reverse to get (query, ref) instead of (ref, query), due to the flip (!)
+            reversed,
+            # query_start instead of ref_start, due to the flip (!)
+            get_aligned_pairs_from_cigar(
+                decode_cigar(pr.cigar.seq), query_start=left_flank_coord, matches_only=True)
+        )
+    ))
+
+    return ret_q(res)
