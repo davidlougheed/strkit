3c3121c423b071608920de1cd6986730f57ab367
David Lougheed <david.lougheed@gmail.com>
2024-01-25 21:41:03 -0500

    chore: update allowed dependencies + dev reqs

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ setup(
         "numpy>=1.23.4,<1.27",
         "parasail>=1.2.4,<1.4",
         "pyfamsa>=0.3.0,<0.4",
-        "scikit-learn>=1.2.1,<1.4",
-        "scipy>=1.10,<1.12",
+        "scikit-learn>=1.2.1,<1.5",
+        "scipy>=1.10,<1.13",
         "statsmodels>=0.14.0,<0.15",
     ],
     extras_require={
