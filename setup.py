8ad74799b5345080b3d8ecc40697cbcaa675ef39
David Lougheed <david.lougheed@gmail.com>
2024-02-07 15:48:27 -0500

    chore: make strkit_rust_ext/orjson required

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     python_requires="~=3.9",
     install_requires=[
         "Flask>=2.2.5,<3.1",
+        "orjson>=3.9.7,<4",
         "pysam>=0.19,<0.23",
         "numpy>=1.23.4,<1.27",
         "parasail>=1.2.4,<1.4",
@@ -22,13 +23,8 @@ setup(
         "scikit-learn>=1.2.1,<1.5",
         "scipy>=1.10,<1.13",
         "statsmodels>=0.14.0,<0.15",
+        "strkit_rust_ext==0.10.0",
     ],
-    extras_require={
-        "rustdeps": [
-            "orjson>=3.9.7,<4",
-            "strkit_rust_ext==0.8.0",
-        ],
-    },
 
     description="A toolkit for analyzing variation in short(ish) tandem repeats.",
     long_description=long_description,
