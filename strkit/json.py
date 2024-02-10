ac303a033867a8ae2cb2e1cd8bbb9c6fc9e98d93
David Lougheed <david.lougheed@gmail.com>
2024-02-10 12:07:58 -0500

    refact: require orjson in code instead of handling importerror

--- strkit/json.py
+++ strkit/json.py
@@ -1,18 +1,12 @@
+import orjson as json
 from typing import Union
 
 Serializable = Union[dict, list, tuple, str, int, float]
 
-try:
-    import orjson as json
 
-    def dumps_indented(v: Serializable) -> bytes:
-        return json.dumps(v, option=json.OPT_NON_STR_KEYS | json.OPT_INDENT_2)
+def dumps_indented(v: Serializable) -> bytes:
+    return json.dumps(v, option=json.OPT_NON_STR_KEYS | json.OPT_INDENT_2)
 
-except ImportError:
-    import json
-
-    def dumps_indented(v: Serializable) -> bytes:
-        return json.dumps(v, indent=2).encode("utf-8")
 
 __all__ = [
     "json",
