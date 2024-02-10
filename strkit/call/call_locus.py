2c63662dcf5bbbb3f13732894603630272a806fd
David Lougheed <david.lougheed@gmail.com>
2024-02-10 12:09:12 -0500

    fix(call): use best representative sequence as a pseudo-consensus avoids errors from rust-bio

--- strkit/call/call_locus.py
+++ strkit/call/call_locus.py
@@ -1260,7 +1260,7 @@ def call_locus(
 
         if call_data and consensus:
             call_seqs = list(
-                map(lambda a: consensus_seq(map(lambda rr: read_dict_extra[rr]["_tr_seq"], a)), allele_reads)
+                map(lambda a: best_representative(map(lambda rr: read_dict_extra[rr]["_tr_seq"], a)), allele_reads)
             )
 
     peak_data = {
