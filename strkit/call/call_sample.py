b0ced24a236ff5570cef5f35ae9b4a94a669d1fb
David Lougheed <david.lougheed@gmail.com>
2024-06-10 12:39:31 -0400

    perf(call): del jobs when finished with var

--- strkit/call/call_sample.py
+++ strkit/call/call_sample.py
@@ -375,6 +375,7 @@ def call_sample(
             #  - gather the process-specific results for combining
             #  - merge sorted result lists into single sorted list for the current contig
             results: tuple[dict, ...] = tuple(heapq.merge(*(j.get() for j in jobs), key=get_locus_index))
+            del jobs
 
             #  - fix-up phase sets based on phase_set_synonymous
             #     - if determined necessary while traversing the phase set graph, flip all peak data (i.e., if phase
