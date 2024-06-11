5fd559a0d049102055259bbdb5d3cff21fa0c15f
David Lougheed <david.lougheed@gmail.com>
2024-06-11 14:22:01 -0400

    chore: bump version to 0.15.0a21

--- strkit/call/call_sample.py
+++ strkit/call/call_sample.py
@@ -32,7 +32,7 @@ __all__ = [
 
 
 # TODO: Parameterize
-LOG_PROGRESS_INTERVAL: int = 10  # seconds
+LOG_PROGRESS_INTERVAL: int = 120  # seconds
 PROFILE_LOCUS_CALLS: bool = False
 
 NUMERAL_CONTIG_PATTERN = re.compile(r"^(\d{1,2}|X|Y)$")
