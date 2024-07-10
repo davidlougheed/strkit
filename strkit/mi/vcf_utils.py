1f69a81c90394fc7db9fe3c0e1bce2d89e906b2a
David Lougheed <david.lougheed@gmail.com>
2024-07-10 13:15:10 -0400

    perf(mi): close VCFs earlier when getting contigs

--- strkit/mi/vcf_utils.py
+++ strkit/mi/vcf_utils.py
@@ -8,13 +8,13 @@ __all__ = ["VCFCalculatorMixin"]
 class VCFCalculatorMixin:
     @staticmethod
     def get_contigs_from_files(mother_call_file, father_call_file, child_call_file) -> tuple[set, set, set]:
-        mvf = pysam.VariantFile(str(mother_call_file))
-        mc = set(mvf.header.contigs)
+        with pysam.VariantFile(str(mother_call_file)) as mvf:
+            mc = set(mvf.header.contigs)
 
-        fvf = pysam.VariantFile(str(father_call_file))
-        fc = set(fvf.header.contigs)
+        with pysam.VariantFile(str(father_call_file)) as fvf:
+            fc = set(fvf.header.contigs)
 
-        cvf = pysam.VariantFile(str(child_call_file))
-        cc = set(cvf.header.contigs)
+        with pysam.VariantFile(str(child_call_file)) as cvf:
+            cc = set(cvf.header.contigs)
 
         return mc, fc, cc
