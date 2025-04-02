ea4179cac9a3097a349d77929e79fbc63dd98f08
David Lougheed <david.lougheed@gmail.com>
2025-04-02 16:49:17 -0400

    chore(deps): update strkit_rust_ext + dev requirements file

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "scikit-learn>=1.2.1,<1.6",
         "scipy>=1.10,<1.16",
         "statsmodels>=0.14.0,<0.15",
-        "strkit_rust_ext==0.20.0",
+        "strkit_rust_ext==0.20.2",
     ],
 
     description="A toolkit for analyzing variation in short(ish) tandem repeats.",
