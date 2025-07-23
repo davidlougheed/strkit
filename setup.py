8e40ecc5f1245228c821cecf62caf5cfb1636b88
David Lougheed <david.lougheed@gmail.com>
2025-07-23 15:07:16 -0400

    chore(deps): update setup.py to specify strkit_rust_ext 0.21.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         "scikit-learn>=1.2.1,<1.8",
         "scipy>=1.10,<1.16",
         "statsmodels>=0.14.0,<0.15",
-        "strkit_rust_ext==0.20.2",
+        "strkit_rust_ext==0.21.0",
     ],
 
     description="A toolkit for analyzing variation in short(ish) tandem repeats.",
