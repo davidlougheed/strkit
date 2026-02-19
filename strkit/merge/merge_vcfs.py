3a188f02b954f17d6c7216f191f3499f892f53c5
David Lougheed <david.lougheed@gmail.com>
2026-02-19 10:22:05 -0500

    merge work

--- strkit/merge/merge_vcfs.py
+++ strkit/merge/merge_vcfs.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
 
 from pathlib import Path
-from pysam import VariantFile, VariantHeader
-from typing import TYPE_CHECKING
+from pysam import VariantFile
+from typing import TYPE_CHECKING, Callable
 
-from .. import __version__
+from ..call.output import vcf
 from ..logger import get_main_logger
 
 if TYPE_CHECKING:
     from logging import Logger
+    from pysam import VariantHeader, VariantRecord
 
 __all__ = ["merge_vcfs"]
 
@@ -68,6 +69,68 @@ def check_headers(headers: tuple[VariantHeader, ...], logger: Logger):
     # TODO: return some info to help?
 
 
+def merge_str_records(header: VariantHeader, records: list[VariantRecord], logger: Logger) -> VariantRecord:
+    contig = records[0].contig
+
+    refs = tuple(p.ref for p in records)
+
+    common_info_keys = (
+        vcf.VCF_INFO_VT,
+        vcf.VCF_INFO_MOTIF,
+        vcf.VCF_INFO_REFMC,
+        vcf.VCF_INFO_BED_START,
+        vcf.VCF_INFO_BED_END,
+    )
+
+    infos = set(tuple(p.info[k] for k in common_info_keys) for p in records)
+
+    if len(infos) > 1:
+        logger.warning("merge_str_records encountered more than one set of INFO field values")
+        # TODO: more reporting
+
+    if len(set(refs)) > 1:
+        print("TODO ref consolidation!")
+        record_max_anchor = max(records, key=lambda p: p.info["ANCH"])
+        max_anchor = record_max_anchor.info["ANCH"]
+        ref_max_anchor = record_max_anchor.ref
+        start = record_max_anchor.start
+        added_prefix_lengths = tuple(max_anchor - p.info["ANCH"] for p in records)
+
+        # TODO: also consider if there's extra on the end or something?
+        ref = ref_max_anchor
+        # TODO: this misses SNVs that are in the VCF anchor. Maybe STRkit should genotype these.
+        alts = sorted(set(ref_max_anchor[:added_prefix_lengths[i]] + a for i, p in enumerate(records) for a in p.alts))
+    else:
+        ref = refs[0]
+        alts = sorted(set(a for p in records for a in (p.alts or ())), key=lambda a: (len(a), a))
+        start = records[0].start
+
+    # TODO: merge info/format fields
+
+    # all same locus, can merge
+    alleles = (ref, *(alts or (".",)))
+    print(alleles)
+    rec = header.new_record(contig, start=start, alleles=alleles, id=records[0].id)
+
+    common_info = next(iter(infos))
+    for ki, k in enumerate(common_info_keys):
+        rec.info[k] = common_info[ki]
+
+    return rec
+
+
+def merge_snv_records(header: VariantHeader, records: list[VariantRecord], logger: Logger) -> VariantRecord:
+    rec = header.new_record()  # TODO
+    print("TODO: SNV MERGE")
+    return rec
+
+
+MERGE_FUNCTION: dict[str, Callable[[VariantHeader, list[VariantRecord], Logger], VariantRecord]] = {
+    vcf.VT_STR: merge_str_records,
+    vcf.VT_SNV: merge_snv_records,
+}
+
+
 def merge_vcfs(paths: tuple[Path, ...]):
     logger = get_main_logger()  # TODO: inject
 
@@ -95,26 +158,12 @@ def merge_vcfs(paths: tuple[Path, ...]):
 
         while any(p is not None for p in ptrs):
             # find compatible records for merging based on ID (requires STRkit to output variant IDs for everything)
-            if len(set(p.id for p in ptrs)) == 1:
-                contig = ptrs[0].contig
-
-                refs = tuple(p.ref for p in ptrs)
-
-                if len(set(refs)) > 1:
-                    print("TODO ref consolidation!")
-                    ref = refs[0]
-                    # TODO: if this happens, we need to also
-                else:
-                    ref = refs[0]
-
-                print(refs)
-
-                # all same locus, can merge
-                rec = merged_header.new_record(contig, alleles=(ref,))
-                pass
+            if all(p is not None for p in ptrs) and len(set(p.id for p in ptrs)) == 1:
+                print(MERGE_FUNCTION[ptrs[0].info["VT"]](merged_header, ptrs, logger), end="")
                 ptrs = [next(i, None) for i in iters]
             else:
-                # we have a record which doesn't apply to all samples
+                # we have a record which doesn't apply to all samples - either some VCFs are finished, or the pointers
+                # are at different points in the file.
                 print("TODO some samples!")
                 # TODO: selectively advance
 
